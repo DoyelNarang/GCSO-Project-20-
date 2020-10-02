d0899b4c6b86a329cc61e1de757ab52a889f71cd
DoyelNarang <71204816+DoyelNarang@users.noreply.github.com>
2020-10-02 11:26:43 +0530

    Add files via upload

---
+++ GCSO SIMULATOR/sketch.js
@@ -0,0 +1,56 @@
+var car, wall;
+var speed, weight
+
+function setup() { 
+createCanvas(1600,400);
+car = createSprite(50, 200, 50, 50);
+car.shapeColor ="rgb(51,255,255)";
+car.debug = true; 
+
+wall = createSprite(1500, 200, 200, height/2);
+wall.shapeColor = color(80,80,80);
+wall.debug = true; 
+
+speed = random(55,90);
+weight = random(400,1500);
+
+car.velocityX = speed;
+} 
+
+function draw() {
+background(0,0,0); 
+   
+if(wall.x - car.x < car.width+wall.width/2 ){
+car.velocityX = 0;
+var deformation = 0.5* weight* speed* speed/22509;
+
+if(deformation>180){
+car.shapeColor=color(255,0,0);
+fill("rgb(255)");
+textSize(45);
+textFont("Old English text Mt");
+text("You are not Safe !!", 600,200);
+}
+
+if(deformation<180 && deformation>100){
+car.shapeColor=color(230,230,0);
+fill("rgb(255)");
+textSize(45);
+textFont("Old English text Mt");
+text("You are not Safe !!", 600,200);
+}
+
+if(deformation<100){
+car.shapeColor=color(0,255,0);
+fill("rgb(255)");
+textSize(45);
+textFont("Old English text Mt");
+text("You are Safe !!", 500,200);
+}
+}
+
+
+         
+drawSprites(); 
+
+}
